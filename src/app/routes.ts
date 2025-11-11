da88259ca279fe3c3d4b98554cc9ae17302b4211
alan80avalos <alejando.rodriguez@iteso.mx>
2025-11-10 21:39:57 -0600

    Crud operations without all testing

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,12 +2,13 @@ import { Router, json } from "express";
 import authRoutes from "./auth/auth.routes"
 import meetingsRoutes from "./meetings/meetings.routes"
 import usersRoutes from "./users/users.routes"
-
+import OrganizationRoutes from "./organizations/org.routes"
 const router = Router();
 
 router.use(json());
 router.use("/auth", authRoutes);
-router.use("/meetings", meetingsRoutes);
 router.use("/users", usersRoutes);
+router.use("/meetings", meetingsRoutes);
+router.use("/organizations", OrganizationRoutes);
 
 export default router;
--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -1,17 +1,73 @@
 import { Request, Response } from "express";
+import User from "./users.model";
+import { IUser } from "../interfaces/user";
+import { Types } from "mongoose";
 
-export const getPersonalUser = (req: Request, res: Response) => {
-    res.status(200).json({ message: "Getting personal profile." });
+export const getPersonalUser = async (req: Request, res: Response) => {
+    try {
+        const userId = (req as any).user?.id;//sino sale error
+        if (!userId) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+        const me = await User.findById(userId).select("-password_hash");//-password_hash para no dar la contraseña
+        if (!me) {
+            return res.status(404).json({ message: "User no found" });
+        }
+        res.status(200).json(me);
+    } catch (error) {
+        res.status(500).json({ message: "Error getting personal users"});
+    }
+    //res.status(200).json({ message: "Getting personal profile." });
 }
 
-export const updatePersonalUser = (req: Request, res: Response) => {
-    res.status(200).json({ message: "Updating personal profile." });
+export const updatePersonalUser = async (req: Request, res: Response) => {
+    try {
+        const userId = (req as any).user?.id;
+        if (!userId) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+        const updates: Partial<IUser> = req.body;
+        const updated = await User.findByIdAndUpdate(userId, updates, { new: true }).select("-password_hash");
+        if(!updated) {
+            return res.status(404).json({ message: "User no found"});
+        }
+        res.status(200).json(updated);
+    } catch (error) {
+        res.status(500).json({ message: "Error updating user"});
+    }
+    //res.status(200).json({ message: "Updating personal profile." });
 }
 
-export const getUserById = (req: Request, res: Response) => {
-    res.status(200).json({ message: "Getting user by ID." });
+export const getUserById = async (req: Request, res: Response) => {
+    try {
+        const { userId } = req.params;
+        if(!Types.ObjectId.isValid(userId)){
+            return res.status(400).json({ message: "Invalid user id"});
+        }
+        const user = await User.findById(userId).select("-password_hash");
+        if(!user){
+            return res.status(404).json({ message: "User no found"});
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: "Error getting user by ID"});
+    }
+    //res.status(200).json({ message: "Getting user by ID." });
 }
 
-export const deleteUserById = (req: Request, res: Response) => {
-    res.status(200).json({ message: "Deleting user by ID." });
+export const deleteUserById = async (req: Request, res: Response) => {
+    try {
+        const { userId } = req.params;
+        if (!Types.ObjectId.isValid(userId)){
+            return res.status(400).json({ message: "Invalid user id" });
+        } 
+        const deleted = await User.findByIdAndDelete(userId);
+        if(!deleted) {
+            return res.status(404).json({ message: "User deleted"});
+        }
+        res.status(204).json({ message: "User deleted"});
+    } catch (error) {
+        res.status(500).json({ message: "Error deleting user"});
+    }
+    //res.status(200).json({ message: "Deleting user by ID." });
 }
