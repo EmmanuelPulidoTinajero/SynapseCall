3c300065cea35f8fdb80de4398f4c9d55b8a587b
Pardosito <ianrdzwong@gmail.com>
2025-12-03 13:12:01 -0600

    FIX: all meeting files downloaded using original s3 bucket name

--- src/app/utils/s3.utils.ts
+++ src/app/utils/s3.utils.ts
@@ -1,6 +1,7 @@
 import { S3, GetObjectCommand, ListObjectsV2Command, ListObjectsV2CommandOutput } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { s3Client } from "../middlewares/upload";
+import { all } from "axios";
 
 export async function listAllS3Keys(prefix: string): Promise<string[]> {
       const allKeys: string[] = [];
@@ -37,10 +38,13 @@ export async function getS3DownloadLink(keys: string[]): Promise<S3Link[]> {
   for (const key of keys) {
     const parts = key.split("---");
     const filename = parts[parts.length - 1];
+    const encodedFilename = encodeURIComponent(filename);
+    const disposition = `attachment; filename="${filename}"; filename*=UTF-8''${encodedFilename}`;
+
     const command = new GetObjectCommand({
       Bucket: "synapse-call",
       Key: key,
-      ResponseContentDisposition: 'attachment; filename ="' + filename + '"'
+      ResponseContentDisposition: disposition
     });
 
     try {
